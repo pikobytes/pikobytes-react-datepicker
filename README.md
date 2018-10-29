3250fbc35448546f85c9c18e7c8a370108220fa3
Jacob Mendt <jacobmendt@googlemail.com>
2018-10-29 09:29:02 +0100

    Setup repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# pikobytes-app-template
+
+This repository contains the basic setup for a [create-react-app](https://github.com/facebook/create-react-app) based web application.
---
+++ src/componentapp.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './componentapp.css';
+
+/**
+ * App
+ */
+export default class App extends Component {
+  /**
+   * Renders the component
+   */
+  render() {
+    return <div className="container template-app">
+      <h4 className="title is-4">ToDo:</h4>
+    </div>;
+  }
+}
